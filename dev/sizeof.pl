7d899ceb52163d828f07a6d2d6b35a60b13fd5f5
chansen <chansen@cpan.org>
2013-11-25 00:38:17 +0100

    Added sizeof.pl which compares memory usage

---
+++ dev/sizeof.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use DateTime      qw[];
+use Time::Moment  qw[];
+use Time::Piece   qw[];
+
+use Devel::Size   qw[total_size];
+
+my $tm = Time::Moment->now;
+my $tp = Time::Piece::localtime();
+my $dt = DateTime->now;
+
+printf "Time::Moment ............... : %4d B\n", total_size($tm);
+printf "Time::Piece ................ : %4d B\n", total_size($tp);
+printf "DateTime ................... : %4d B\n", total_size($dt);
+printf "DateTime w/o zone and locale : %4d B\n", total_size do {
+    delete @{$dt}{qw(time_zone locale)}; $dt;
+};
+
