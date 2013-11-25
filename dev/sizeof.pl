e1c0a5a431771eed0ca6b85e160a7faf151f6df4
chansen <chansen@cpan.org>
2013-11-25 09:41:04 +0100

    added sizeof localtime() for comparison

--- dev/sizeof.pl
+++ dev/sizeof.pl
@@ -11,9 +11,11 @@ use Devel::Size   qw[total_size];
 my $tm = Time::Moment->now;
 my $tp = Time::Piece::localtime();
 my $dt = DateTime->now;
+my $lt = [localtime()];
 
 printf "Time::Moment ............... : %4d B\n", total_size($tm);
 printf "Time::Piece ................ : %4d B\n", total_size($tp);
+printf "localtime() ................ : %4d B\n", total_size($lt) - total_size([]);
 printf "DateTime ................... : %4d B\n", total_size($dt);
 printf "DateTime w/o zone and locale : %4d B\n", total_size do {
     delete @{$dt}{qw(time_zone locale)}; $dt;
