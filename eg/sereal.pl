a3b72078e06eda57c0141af6a2b6f01f5657a681
chansen <chansen@cpan.org>
2014-01-08 21:58:32 +0100

    Added Sereal example

---
+++ eg/sereal.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Time::Moment qw[];
+use Sereal       2.030 qw[encode_sereal decode_sereal];
+
+my @moments = (Time::Moment->now, Time::Moment->now_utc);
+my $encoded = encode_sereal([ @moments ], { freeze_callbacks => 1 });
+my $decoded = decode_sereal($encoded);
+
+foreach my $time (@$decoded) {
+    print $time->to_string, "\n";
+}
+
