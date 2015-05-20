chansen/p5-time-moment

commit 60591efc3a446d3d13323c81c9c910987a969ffb

Implemented ->with_second_of_day

src/moment.c
@@ -77,6 +77,12 @@ THX_check_second(pTHX_ IV v) {
 }
 
 static void
+THX_check_second_of_day(pTHX_ IV v) {
+    if (v < 0 || v > 86399)
+        croak("Parameter 'second' is out of the range [0, 86399]");
+}
+
+static void
 THX_check_millisecond(pTHX_ IV v) {
     if (v < 0 || v > 999)
         croak("Parameter 'millisecond' is out of the range [0, 999]");
@@ -346,6 +352,17 @@ THX_moment_with_second(pTHX_ const moment_t *mt, IV v) {
 }
 
 moment_t
+THX_moment_with_second_of_day(pTHX_ const moment_t *mt, IV v) {
+    moment_t r;
+
+    THX_check_second_of_day(aTHX_ v);
+    r.sec    = mt->sec + (v - moment_second_of_day(mt));
+    r.nsec   = mt->nsec;
+    r.offset = mt->offset;
+    return r;
+}
+
+moment_t
 THX_moment_with_millisecond(pTHX_ const moment_t *mt, IV msec) {
     moment_t r;
 
@@ -548,6 +565,8 @@ THX_moment_with_component(pTHX_ const moment_t *mt, moment_component_t c, IV v) 
             return THX_moment_with_minute(aTHX_ mt, v);
         case MOMENT_COMPONENT_SECOND:
             return THX_moment_with_second(aTHX_ mt, v);
+        case MOMENT_COMPONENT_SECOND_OF_DAY:
+            return THX_moment_with_second_of_day(aTHX_ mt, v);
         case MOMENT_COMPONENT_MILLISECOND:
             return THX_moment_with_millisecond(aTHX_ mt, v);
         case MOMENT_COMPONENT_MICROSECOND:

Moment.xs
@@ -578,6 +578,7 @@ with_year(self, value)
     Time::Moment::with_hour           =  MOMENT_COMPONENT_HOUR
     Time::Moment::with_minute         =  MOMENT_COMPONENT_MINUTE
     Time::Moment::with_second         =  MOMENT_COMPONENT_SECOND
+    Time::Moment::with_second_of_day  =  MOMENT_COMPONENT_SECOND_OF_DAY
     Time::Moment::with_millisecond    =  MOMENT_COMPONENT_MILLISECOND
     Time::Moment::with_microsecond    =  MOMENT_COMPONENT_MICROSECOND
     Time::Moment::with_nanosecond     =  MOMENT_COMPONENT_NANOSECOND

lib/Time/Moment.pod
@@ -35,6 +35,7 @@ Time::Moment - Represents a date and time of day with an offset from UTC
     $hour         = $tm->hour;                          # [0, 23]
     $minute       = $tm->minute;                        # [0, 59]
     $second       = $tm->second;                        # [0, 59]
+    $second       = $tm->second_of_day;                 # [0, 86_399]
     $millisecond  = $tm->millisecond;                   # [0, 999]
     $microsecond  = $tm->microsecond;                   # [0, 999_999]
     $nanosecond   = $tm->nanosecond;                    # [0, 999_999_999]
@@ -55,6 +56,7 @@ Time::Moment - Represents a date and time of day with an offset from UTC
     $tm2          = $tm1->with_hour($hour);
     $tm2          = $tm1->with_minute($minute);
     $tm2          = $tm1->with_second($second);
+    $tm2          = $tm1->with_second_of_day($second);
     $tm2          = $tm1->with_millisecond($millisecond);
     $tm2          = $tm1->with_microsecond($microsecond);
     $tm2          = $tm1->with_nanosecond($nanosecond);
@@ -543,6 +545,13 @@ altered.
     $tm2 = $tm1->with_second($second);
 
 Returns a copy of this instance with the given I<second> of minute [0, 59] 
+altered.
+
+=head2 with_second_of_day
+
+    $tm2 = $tm1->with_second_of_day($second);
+
+Returns a copy of this instance with the given I<second> of day [0, 86_399] 
 altered.
 
 =head2 with_millisecond

src/moment.h
@@ -87,6 +87,7 @@ typedef enum {
     MOMENT_COMPONENT_HOUR,
     MOMENT_COMPONENT_MINUTE,
     MOMENT_COMPONENT_SECOND,
+    MOMENT_COMPONENT_SECOND_OF_DAY,
     MOMENT_COMPONENT_MILLISECOND,
     MOMENT_COMPONENT_MICROSECOND,
     MOMENT_COMPONENT_NANOSECOND,

commit af8f0b78646132201fed7d5a1ac4ab5bbd128151

Bug fix for when Perl is compiled with threads. Bug introduced in 60591ef (0.23) fixes issue #13, reported by Dmytro Zagashev (ZDM).

src/moment.c
@@ -446,7 +446,7 @@ THX_moment_plus_seconds(pTHX_ const moment_t *mt, int64_t v) {
 
 moment_t
 THX_moment_at_utc(pTHX_ const moment_t *mt) {
-    return moment_with_offset_same_instant(aTHX_ mt, 0);
+    return THX_moment_with_offset_same_instant(aTHX_ mt, 0);
 }
 
 moment_t