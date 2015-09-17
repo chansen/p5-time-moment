8250e2d5feeb7b386a9829e3d73928009b5c50f2
chansen <chansen@cpan.org>
2015-09-17 22:08:26 +0200

    Bumped version

--- lib/Time/Moment.pm
+++ lib/Time/Moment.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw[];
 
 BEGIN {
-    our $VERSION = '0.28';
+    our $VERSION = '0.29';
     require XSLoader; XSLoader::load(__PACKAGE__, $VERSION);
 }
 
--- lib/Time/Moment/Adjusters.pm
+++ lib/Time/Moment/Adjusters.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw[];
 
 BEGIN {
-    our $VERSION    = '0.28';
+    our $VERSION    = '0.29';
     our @EXPORT_OK  = qw[ NextDayOfWeek
                           NextOrSameDayOfWeek
                           PreviousDayOfWeek
