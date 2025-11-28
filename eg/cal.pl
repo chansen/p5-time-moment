49e40dd8c1668fa6a7deb651d456a5947107a642
chansen <chansen@cpan.org>
2025-11-28 05:42:19 +0100

    Simplified eg/cal.pl

--- eg/cal.pl
+++ eg/cal.pl
@@ -61,13 +61,10 @@ my $date       = $Moment->minus_weeks($FirstDayOfWeek > $Moment->day_of_week)
                         ->with_day_of_week($FirstDayOfWeek);
 
 while ($date->is_before($next_month)) {
-    my @week;
-    foreach my $day (1..7) {
-        if ($date->is_before($this_month)) {
-            push @week, '  ';
-        }
-        elsif ($date->is_before($next_month)) {
-            push @week, sprintf '%2d', $date->day_of_month;
+    my @week = ('  ') x 7;
+    foreach my $index (0..6) {
+        if (!$date->is_before($this_month) && $date->is_before($next_month)) {
+            $week[$index] = sprintf '%2d', $date->day_of_month;
         }
         $date = $date->plus_days(1);
     }
